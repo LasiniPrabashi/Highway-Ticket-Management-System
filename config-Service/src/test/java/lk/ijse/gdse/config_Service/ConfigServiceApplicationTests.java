5a73eb5e9226d461adb1afcce4a5f166b30ba60c
LasiniPrabashi <lasiniprabashi0327@gmail.com>
2024-06-28 13:12:54 +0530

    create project

---
+++ config-Service/src/test/java/lk/ijse/gdse/config_Service/ConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.gdse.config_Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- vehical_server/src/main/java/com/example/vehical_server/repo/VehicleRepo.java
+++
@@ -1,7 +0,0 @@
-package com.example.vehical_server.repo;
-
-import com.example.vehical_server.entity.Vehicle;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface VehicleRepo extends JpaRepository<Vehicle,String> {
-}
