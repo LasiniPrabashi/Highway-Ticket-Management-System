9054a8f8e3fd6f4c5e26d37700e2ce8f582d3903
LasiniPrabashi <lasiniprabashi0327@gmail.com>
2024-06-28 13:32:39 +0530

    update

--- api-gateway/src/test/java/lk/ijse/gdse/api_gateway/ApiGatewayApplicationTests.java
+++ api-gateway/src/test/java/lk/ijse/gdse/api_gateway/ApiGatewayApplicationTests.java
@@ -1,10 +1,10 @@
-package lk.ijse.gdse.config_Service;
+package lk.ijse.gdse.api_gateway;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ConfigServiceApplicationTests {
+class ApiGatewayApplicationTests {
 
 	@Test
 	void contextLoads() {
